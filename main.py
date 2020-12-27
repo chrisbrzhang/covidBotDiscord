4d70f7ee572ba5f950e31a724b3fdcb6eba46d9d
Chris <zhang.chris89@gmail.com>
2020-12-27 13:55:31 -0800

    main file finished

--- main.py
+++ main.py
@@ -1,48 +1,63 @@
 import discord
 import csv
-import Corona_Reader
 from discord.ext import commands
+import asyncio
+import time 
+import csv
+import pandas as pd 
+import os
+
+
 
 client = commands.Bot(command_prefix = '!')
+f = open('covidBOT_data.csv')
+csv_f = csv.reader(f) 
+
 
 @client.event
 async def on_ready():
     print('Bot is ready')
-
+ 
 @client.event
 async def on_member_join(member):
     print(f'{member} has joined the server')
 
 @client.event
 async def on_member_leave(member):
-    print(f'{member} has left the server')
+    print(f'{member} has left the server')    
+    
+@client.event    
+async def main(): #Insert discord Token here.
+    await client.start('Discord Token')
+    
+@client.command()
+async def ping(ctx):
+    await ctx.send('Ping is ' + str(round(client.latency*1000)) + 'ms' )
+   
+@client.command()
+async def link(ctx):
+    await ctx.send('https://www.worldometers.info/coronavirus/country/')
 
+@client.command()
+async def country(ctx,arg):
+    await ctx.send("Running..... ")
+    os.system('python Corona_Reader.py') #must have a file called Corona_Reader.py in same directry
+    col_list = ["Country", "Total_Cases","New_Cases","Total_Death"]
+    df = pd.read_csv("covidBOT_data.csv", usecols=col_list)
+    country = str(arg)
+    counter = 0
+    for row in range(0,len(df)):
+        if(df["Country"][row]== country and counter == 0):
+            await ctx.send("Total Cases: ")
+            await ctx.send(df["Total_Cases"][row])
+            await ctx.send("New Cases: ")
+            await ctx.send(df["New_Cases"][row])
+            await ctx.send("Total Death ") 
+            await ctx.send(df["Total_Death"][row])
+            counter = counter+1   
+            break
+        elif(row == len(df)-1):
+            await ctx.send("Your Country does not Exist! Use correct Capitals and Spelling")
 
-@client.event
-async def on_message(channel, message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!IM'):
-        Corona_Reader.csvGenerator()
-        f = open('covidNumbers.csv')
-        csv_f = csv.reader(f)
-        msg = 'Current number of cases are: \n'
-        await channel.send(msg)
-        for row in csv_f:
-            await channel.send(('send the csv content'))
-    else:
-        await message.delete()
-
-            
-
-
-##@client.command()
-##async def IM(ctx, arg):
-    ##await ctx.send('https://www.worldometers.info/coronavirus/country/' + arg)
-
-
-
-
-
-client.run('Nzg5NjE4NTgxMzM5MzczNjA5.X90rnA.YVp1N3WXN__xdowuFh4VtAmx98Y')
+loop=asyncio.get_event_loop() 
+loop.run_until_complete(main())
